6962f882a95455353ffdc050d467d9f9ac5e6bda
Jonatha Daguerre Vasconcelos <jonatha@daguerre.com.br>
2021-07-13 21:35:25 -0300

    docs(singleton.py): improve the singleton.py docstring
    add a decorated class as example

--- patterns/singleton.py
+++ patterns/singleton.py
@@ -4,24 +4,50 @@ Singleton Pattern
 ********************
 
 Description from Wikipedia:
+
 *"the singleton pattern is a software design pattern that
 restricts the instantiation of a class to one 'single' instance.
 This is useful when exactly one object is needed to coordinate
 actions across the system."*
-https://en.wikipedia.org/wiki/Singleton_pattern
 
+.. warning:: *"Critics consider the singleton to be an anti-pattern
+    in that it is frequently used in scenarios where it is not beneficial,
+    introduces unnecessary restrictions in situations where a sole instance
+    of a class is not actually required, and introduces global state into an application."*
 
-The more pythonic way to do it is by creating
-a decorator to store the decorated class instance.
+Source: https://en.wikipedia.org/wiki/Singleton_pattern
+
+The more pythonic way to implement a singleton is by creating
+a decorator to store the decorated class instances.
 """
 
 
 def singleton(cls):
     """
-    Create a single instance of a class.
+    This decorator ensures that only one instance of
+    the decorated class will be created in runtime.
 
-    :param cls: a class to be decorated
+    :param cls: a class to be decorated as singleton
     :type cls: class
+
+    Example:
+        A decorated class that displays its own `id(self)`
+        will always return the same value.
+
+    >>> @singleton
+    >>> class SingletonExample:
+    >>>    def __init__(self):
+    >>>        self.own_id: int = id(self)
+    >>>    def __repr__(self):
+    >>>        return f"{self.__class__.__name__}(id={self.own_id})"
+    >>>
+    >>> SingletonExample()
+    SingletonExample(id=140472046362688)
+    >>> SingletonExample()
+    SingletonExample(id=140472046362688)
+    >>> SingletonExample() is SingletonExample()
+    True
+
     """
     instances = {}
 
@@ -42,7 +68,7 @@ class SingletonExample:
     """
 
     def __init__(self):
-        self.value: int = id(self)
+        self.own_id: int = id(self)
 
     def __repr__(self):
-        return f"{self.__class__.__name__}(value={self.value})"
+        return f"{self.__class__.__name__}(id={self.own_id})"
